496fc9a2a0019f0866f9c414a063001d4c36c84a
Paloma Barroso <palomapbarroso@gmail.com>
2022-11-24 21:37:08 -0300

    Initial commit

---
+++ src/variables/bakery.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    var milkPrice float32 = 2.99
+    var eggPrice float32 = 3.99
+    var breadPrice float32 = 0.99
+
+    fmt.Println("The milk Price is: R$", milkPrice)
+    fmt.Println("The egg price is: R$", eggPrice)
+    fmt.Println("The bread price is: R$", breadPrice)
+}
---
+++ src/variables/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name string = "Paloma"
+	var age int = 29
+	var version float32 = 1.0
+
+	fmt.Println("Hello ", name)
+	fmt.Println("Your age is: ", age)
+	fmt.Println("The version of this program is: ", version)
+}
